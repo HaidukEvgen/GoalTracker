7f09e868e46f26398e3600ddb1e392ea9264c15c
kanura56@gmail.com <kanura56@gmail.com>
2023-07-03 20:33:16 +0300

    Model file

---
+++ GoalTracker/Models/Goal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GoalTracker.Models
+{
+    public class Goal
+    {
+        public string Title { get; set; }
+
+        public DateTime? Deadline { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string Description { get; set; }
+
+        public int Aim { get; set; }
+            
+        public string UnitOfMeasure { get; set; }
+
+        public int CurrentAchievement { get; set; } = 0;
+
+        public double Progress => (double)CurrentAchievement * 100 / Aim ;
+
+        public bool IsValid()
+        {
+            return true;
+        }
+    }
+}
