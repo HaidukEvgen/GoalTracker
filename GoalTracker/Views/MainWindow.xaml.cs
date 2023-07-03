a25ad95dcc2187ec2ee1b148d1c29dc47da7c001
kanura56@gmail.com <kanura56@gmail.com>
2023-07-03 20:32:56 +0300

    Windows files

---
+++ GoalTracker/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using GoalTracker.Enums;
+using GoalTracker.ViewModels;
+
+namespace GoalTracker.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var viewModel = new MainWindowViewModel();
+            DataContext = viewModel;
+        }
+    }
+}
